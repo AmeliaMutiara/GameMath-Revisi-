f62b09318ef3edf34550d3d57fd9cbb81c266221
AmeliaMutiara <98964756+AmeliaMutiara@users.noreply.github.com>
2022-11-06 12:29:56 +0700

    Add files via upload

---
+++ GameMath/src/soal/Penjumlahan.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soal;
+
+/**
+ *
+ * @author AMEL
+ */
+public class Penjumlahan {
+    public int getJawaban(int X, int Y){
+        return X + Y;
+    }
+}
